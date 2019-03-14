19992604464fc9648ff68c5715702fdf16d29c07
Will Holsten <willholsten@wills-mbp.home>
2019-03-14 09:22:44 +0000

    about content

--- src/components/about.js
+++ src/components/about.js
@@ -8,17 +8,22 @@ class About extends React.Component {
       <div className="section-container white-bg">
         <Container>
           <h3 className="section-header">A little about me</h3>
-          <Row>
+          <Row className="about-content">
             <Col md={6}>
               <p>Hey, I'm Will, a front end developer based in London, UK.</p>
               <p>
                 Originally from Australia, I graduated from The University of
-                Adelaide with a Bachelor of Design Studies. Having now worked in
-                both of these fields for a number of years, I have extensive
-                experience working in multi-functional, agile team environments
-                with a primary focus on eCommerce solutions.
+                Adelaide with a Bachelor of Design Studies. Since then I have
+                grown a huge interest in UX design and front end development.
+                Having now worked in both of these fields for a number of years,
+                I have extensive experience working in multi-functional, agile
+                team environments with a primary focus on eCommerce solutions.
               </p>
-              <p>Besides my interests in technology and the web I also enjoy</p>
+              <p>
+                Besides my interests in technology and the web I also enjoy
+                watching football, generally being active and following various
+                dog breeds on Instagram.
+              </p>
             </Col>
 
             <Col md={4} className="ml-auto">
