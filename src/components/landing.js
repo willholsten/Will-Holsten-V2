159e95d2c25415d2baf087216e4aa14987be174f
Will Holsten <willholsten@wills-mbp.home>
2019-03-25 09:14:04 +0000

    landing/hero alignment

--- src/components/landing.js
+++ src/components/landing.js
@@ -6,28 +6,26 @@ import WillHolstenCV from '../images/WillHolstenCV.pdf'
 export default class Landing extends React.Component {
   render() {
     return (
-      <div className="landing green-bg">
-        <Container>
-          <div className="hero">
-            <h1 className="hero-name">Will Holsten</h1>
-            <p className="hero-description">
-              Front end development,
-              <br /> London
-            </p>
-            <ButtonToolbar className="justify-content-center">
-              <Button href="#contact" variant="primary">
-                Get in touch
-                <FontAwesomeIcon className="arrow-right" icon="arrow-right" />
-              </Button>
-              <Button href={WillHolstenCV} target="_blank" variant="primary">
-                <FontAwesomeIcon className="download" icon="download" />
-                CV
-              </Button>
-            </ButtonToolbar>
-            <p className="logotype-w">W</p>
-          </div>
-        </Container>
-      </div>
+      <Container className="landing green-bg">
+        <div className="hero">
+          <h1 className="hero-name">Will Holsten</h1>
+          <p className="hero-description">
+            Front end development,
+            <br /> London
+          </p>
+          <ButtonToolbar className="justify-content-center">
+            <Button href="#contact" variant="primary">
+              Get in touch
+              <FontAwesomeIcon className="arrow-right" icon="arrow-right" />
+            </Button>
+            <Button href={WillHolstenCV} target="_blank" variant="primary">
+              <FontAwesomeIcon className="download" icon="download" />
+              CV
+            </Button>
+          </ButtonToolbar>
+          <p className="logotype-w">W</p>
+        </div>
+      </Container>
     )
   }
 }
